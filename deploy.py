0abafda26d359d276fd32c1a28a44d6b8a3948a4
Quadtree <quadtree@gmail.com>
2019-05-04 00:18:25 -0700

    Content added

---
+++ deploy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import zipfile
+import os
+import subprocess
+
+with zipfile.ZipFile('/tmp/new-version.zip', 'w') as z:
+    z.write(os.path.join(os.path.dirname(os.path.realpath(__file__))), 'index.js')
+
+subprocess.run(['aws', '--region=us-east-1', 'lambda', 'update-function-code', '--zip-file=fileb:///tmp/new-version.zip', '--function-name=recordScore'])
---
+++ index.js
@@ -0,0 +1,90 @@
+exports.handler = async (event) => {
+    return new Promise((resolve, reject) => {
+
+        console.log(event.body);
+
+        params = {};
+
+        for (let seg of event.body.split('&')){
+          let parts = seg.split('=');
+          params[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+        }
+
+        // Load the AWS SDK for Node.js
+        var AWS = require('aws-sdk');
+        // Set the region
+        AWS.config.update({region: 'us-east-1'});
+
+        // Create the DynamoDB service object
+        var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+        const id = Math.round(Math.random() * 10000000000);
+
+        var item = {};
+
+        for (let k in params){
+          if (k != 'game' && k != 'score' && k != 'level'){
+            item[k] = {S: params[k]};
+          }
+        }
+
+        item['GameLevel'] = {S: `${params['Game']}-${params['Level']}`};
+        item['Game'] = {S: `${params['Game']}`};
+        item['Level'] = {S: `${params['Level']}`};
+        item['Id'] = {N: id.toString()};
+        item['Score'] = {N: params['Score']};
+
+        var params = {
+          TableName: 'Scores',
+          Item: item
+        };
+
+
+
+        const response = {
+            statusCode: 200,
+            body: `???`
+        };
+
+        ddb.putItem(params, function(err, data) {
+          if (err) {
+            console.log("Error", err);
+            reject(`Error: ${err}`);
+          } else {
+            console.log("Success", data);
+
+            var params = {
+              ExpressionAttributeValues: {
+               ":v1": item['GameLevel']
+              },
+              KeyConditionExpression: "GameLevel = :v1",
+              ScanIndexForward: false,
+              IndexName: "GameLevel-Score-index",
+              TableName: "Scores",
+              Limit: 3,
+            };
+            ddb.query(params, function(err, data) {
+              if (err){
+                reject(err);
+              } else {
+                console.log(data);
+
+                let responseJson = [];
+
+                for (let row of data.Items){
+                  let responseRow = {};
+                  for (let k in row){
+                    responseRow[k] = row[k].S ? row[k].S : row[k].N;
+                  }
+                  responseJson.push(responseRow);
+                }
+
+                response.body = JSON.stringify(responseJson);
+
+                resolve(response);
+              }
+             });
+          }
+        });
+    });
+};
