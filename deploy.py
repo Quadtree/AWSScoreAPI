969132d9f1a6c92fc1d59346714f577356ed8d5b
Quadtree <quadtree@gmail.com>
2019-05-04 00:19:17 -0700

    Minor fix

--- deploy.py
+++ deploy.py
@@ -5,6 +5,6 @@ import os
 import subprocess
 
 with zipfile.ZipFile('/tmp/new-version.zip', 'w') as z:
-    z.write(os.path.join(os.path.dirname(os.path.realpath(__file__))), 'index.js')
+    z.write(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'index.js'), 'index.js')
 
 subprocess.run(['aws', '--region=us-east-1', 'lambda', 'update-function-code', '--zip-file=fileb:///tmp/new-version.zip', '--function-name=recordScore'])
